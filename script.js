d989796d0f69b9ad96af0f732b5b0dff70f44c86
Davi Ricardo <142528743+DavirTI@users.noreply.github.com>
2024-12-27 23:43:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function mostrarMensagem() {
+    const mensagem = document.getElementById("mensagem");
+    mensagem.style.display = "block";
+  }
+  function mostrarMensagem() {
+    const mensagem = document.getElementById("mensagem");
+    mensagem.style.display = "block";
+  }
+  
